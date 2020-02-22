b7b8ff34e2ff26f64e76a993eb37612a56e8d83c
RenzHoly <renzholy@gmail.com>
2020-02-23 01:15:30 +0800

    Update color-wheel.js

--- color-wheel.js
+++ color-wheel.js
@@ -1,24 +1,24 @@
 export function generateColorWheel(size) {
   const colors = [
-    '#B71C1C',
-    '#880E4F',
-    '#4A148C',
-    '#311B92',
-    '#1A237E',
-    '#0D47A1',
-    '#01579B',
-    '#006064',
-    '#004D40',
-    '#1B5E20',
-    '#33691E',
-    '#827717',
-    '#F57F17',
-    '#FF6F00',
-    '#E65100',
-    '#BF360C',
-    '#3E2723',
-    '#212121',
-    '#263238',
+    '#F44336',
+    '#E91E63',
+    '#9C27B0',
+    '#673AB7',
+    '#3F51B5',
+    '#2196F3',
+    '#03A9F4',
+    '#00BCD4',
+    '#009688',
+    '#4CAF50',
+    '#8BC34A',
+    '#CDDC39',
+    '#FFEB3B',
+    '#FFC107',
+    '#FF9800',
+    '#FF5722',
+    '#795548',
+    '#9E9E9E',
+    '#607D8B',
     '#000000',
   ]
   const colorWheel = document.createElement('div')
@@ -35,5 +35,14 @@ export function generateColorWheel(size) {
     sector.style.clipPath = 'polygon(50% 50%, 0% 0%, 25% 0%)'
     colorWheel.appendChild(sector)
   }
+  const center = document.createElement('div')
+  center.style.position = 'absolute'
+  center.style.left = `${size >> 2}px`
+  center.style.top = `${size >> 2}px`
+  center.style.background = 'white'
+  center.style.width = `${size >> 1}px`
+  center.style.height = `${size >> 1}px`
+  center.style.borderRadius = '100%'
+  colorWheel.appendChild(center)
   return colorWheel
 }
